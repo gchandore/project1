9d99810f30b42cfb50fb94aa9de1615ea00dd0ce
gchandore <gchandore@yahoo.com>
2019-04-17 20:45:45 +0000

    This is my first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+/* This is a simple Java program.
+ 
+FileName : "Hello.java". */
+class Hello{  
+public static void main(String[] args){  
+System.out.println("This is java app \n by using Docker");  
+}  
+}  
